d8b1f46b3efe6833ad4b0cec877885168676b4dc
Milton Castro <miltonmc@gmail.com>
2020-03-04 07:58:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Testes Unitários (Projeto)
+
+Projeto da disciplina de Testes Unitários da Especialização em Testes Ágeis, turma
+2018.2.
+
+## Projeto
+
+Cada aluno deve criar um fork desse projeto e em seu repositório do GitHub escrever os testes
+unitários para as classes:
+- LeapYearCalculator
+- PeopleComparator
+
+## Avaliação
+
+ Cada teste unitário será avaliado quanto aos seguintes critérios:
+- Qualidade dos testes:
+    - Isolamento
+    - Nomenclatura do teste
+- Correta utilização de mocks, stubs, spies, fake e dummy.
+
+### Nota
+
+A nota será calculada da seguinte forma:
+- **7 pontos**: Escrita dos casos de testes unitários.
+- **3 pontos**: Cobertura de teste, segundo critérios utilizados
+pelo sonar.
+    
+#### Ponto extra:
+
+Fará juz ao ponto exta o aluno que: 
+- **0.5 pontos extra**: Através do GitHub Actions executar os testes do projeto
+e reportar a cobertura numa instância do SonarCloud.
+- **0.5 pontos extra**: Utilizar a dependência do Mockito para criação de
+dublês de teste, quando houver a necessidade.
---
+++ src/main/java/school/cesar/eta/unit/PeopleComparator.java
@@ -0,0 +1,62 @@
+package school.cesar.eta.unit;
+
+import school.cesar.eta.unit.entity.Person;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class PeopleComparator {
+
+    public LocalDate getCurrentDate(){
+        return LocalDate.now();
+    }
+
+    public boolean isTodayPersonsBirthDay(Person person){
+        LeapYearCalculator leapYearCalculator = new LeapYearCalculator();
+        LocalDate now = getCurrentDate();
+        LocalDate birthday = person.getBirthday();
+
+        if (now.getMonth() == birthday.getMonth() && now.getDayOfMonth() == birthday.getDayOfMonth()){
+            return true;
+        }
+
+        return !leapYearCalculator.isCurrentYearLeapYear()
+                && now.getDayOfMonth() == 1 && now.getMonth() == Month.MARCH
+                && birthday.getDayOfMonth() == 29 && birthday.getMonth() == Month.FEBRUARY;
+    }
+
+    public boolean isSameFamily(Person firstPerson, Person secondPerson){
+        if(firstPerson.getName().equals(secondPerson.getName())){
+            return false;
+        }
+        if(!firstPerson.getLastName().equals(secondPerson.getLastName())){
+            return false;
+        }
+        if(!firstPerson.getCity().equals(secondPerson.getCity())){
+            return false;
+        }
+        return firstPerson.getState().equals(secondPerson.getState());
+    }
+
+    public boolean isSamePerson(Person firstPerson, Person secondPerson) {
+        if(firstPerson == secondPerson){
+            throw new RuntimeException("Same instance!");
+        }
+        if(!firstPerson.getName().equals(secondPerson.getName())){
+            return false;
+        }
+        if(!firstPerson.getLastName().equals(secondPerson.getLastName())){
+            return false;
+        }
+        if(!firstPerson.getBirthday().equals(secondPerson.getBirthday())){
+            return false;
+        }
+        if(!firstPerson.getMaritalStatus().equals(secondPerson.getMaritalStatus())){
+            return false;
+        }
+        if(!firstPerson.getCity().equals(secondPerson.getCity())){
+            return false;
+        }
+        return firstPerson.getState().equals(secondPerson.getState());
+    }
+}
