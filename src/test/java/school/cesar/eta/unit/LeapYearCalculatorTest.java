e6e699af67311916a761180b4735e744b73b2600
rods7on <richard.rodston@gmail.com>
2020-03-05 14:31:45 -0300

    Initial

--- src/test/java/school/cesar/eta/unit/LeapYearCalculatorTest.java
+++ src/test/java/school/cesar/eta/unit/LeapYearCalculatorTest.java
@@ -6,15 +6,15 @@ import org.junit.jupiter.api.Test;
 public class LeapYearCalculatorTest {
 
 
-    @Test //é ano comum
-    public void Year_AnoComum_true() {
-        Assertions.assertEquals("Não é Bisexto",
-                LeapYearCalculator.year(2100));
-    }
-
-    @Test
-    public void Year_AnoBisexto_true(){
-        Assertions.assertEquals("Ano Bisexto",
-                LeapYearCalculator.year(1996));
-    }
+//    @Test //é ano comum
+//    public void Year_AnoComum_true() {
+//        Assertions.assertEquals("Não é Bisexto",
+//                LeapYearCalculator.year(2100));
+//    }
+//
+//    @Test
+//    public void Year_AnoBisexto_true(){
+//        Assertions.assertEquals("Ano Bisexto",
+//                LeapYearCalculator.year(1996));
+//    }
 }
