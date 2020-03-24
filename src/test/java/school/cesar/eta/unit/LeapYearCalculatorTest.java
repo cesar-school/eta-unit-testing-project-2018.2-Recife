d86eb2e002e6035b4bd95923607d75128f175992
Jhonatan Castro <jhonatan.castro@compasso.com.br>
2020-03-23 22:15:12 -0300

    Configuração inicial do pom Criação dos primeiros testes para a classe LeapYearCalculatorTest

---
+++ src/test/java/school/cesar/eta/unit/LeapYearCalculatorTest.java
@@ -0,0 +1,53 @@
+package school.cesar.eta.unit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Year;
+
+public class LeapYearCalculatorTest {
+    public Integer currentYear;
+    public LeapYearCalculator leapYearCalculator;
+
+    @BeforeEach
+    public void setUp(){
+        currentYear = null;
+        leapYearCalculator = new LeapYearCalculator(){
+            @Override
+            public int getCurrentYear(){
+                return currentYear;
+            }
+        };
+    }
+
+    @Test
+    public void year_CorfirmCurrentYear_YearCurrent(){
+        Assertions.assertEquals(Year.now().getValue(), new LeapYearCalculator().getCurrentYear());
+    }
+
+    @Test
+    public void year_NotDivisibleByFour_LeapYearFalse(){
+        currentYear = 2003;
+        Assertions.assertFalse(leapYearCalculator.isCurrentYearLeapYear());
+    }
+
+    @Test
+    public void year_DivisibleByFourButNotDivisibleByAHundred_LeapYearTrue(){
+        currentYear = 2020;
+        Assertions.assertTrue(leapYearCalculator.isCurrentYearLeapYear());
+    }
+
+    @Test
+    public void year_DivisibleByFourAndByOneHundredButNotDivisibleByFourHundreds_LeapYearFalse(){
+        currentYear = 100;
+        Assertions.assertFalse(leapYearCalculator.isCurrentYearLeapYear());
+    }
+
+    @Test
+    public void year_DivisibleByFourAndByOneHundredAndByFourHundreds_LeapYearFalse(){
+        currentYear = 400;
+        Assertions.assertTrue(leapYearCalculator.isCurrentYearLeapYear());
+    }
+}
+
