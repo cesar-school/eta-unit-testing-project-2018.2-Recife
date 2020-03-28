974b4d4c7eabd328f23a57ebbae5a7816b82ff7a
rods7on <richard.rodston@gmail.com>
2020-03-28 11:48:51 -0300

    LeapYear testes

--- src/test/java/school/cesar/eta/unit/LeapYearCalculatorTest.java
+++ src/test/java/school/cesar/eta/unit/LeapYearCalculatorTest.java
@@ -3,6 +3,9 @@ package school.cesar.eta.unit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sun.awt.geom.AreaOp;
+
+import java.time.Year;
 
 // * if (year is not divisible by 4) then (it is a common year)
 // * else if (year is not divisible by 100) then (it is a leap year)
@@ -15,7 +18,8 @@ public class LeapYearCalculatorTest {
     public LeapYearCalculator calc;
 
     @BeforeEach
-    public void start(){
+    public void setUp(){
+        ano = null;
         calc = new LeapYearCalculator(){
             @Override
             public int getCurrentYear(){
@@ -24,29 +28,37 @@ public class LeapYearCalculatorTest {
         };
     }
 
+    //Ano atual
+    @Test
+    public void verificarAnoAtual(){
+        Assertions.assertEquals(Year.now().getValue(), new LeapYearCalculator().getCurrentYear());
+//        System.out.println(Year.now());
+    }
+
+
     //Anos normais
     @Test
-    public void naoDivisivelPor4_anoNormal() {
+    public void naoDivisivelPor4() {
         ano = 2003;
         Assertions.assertFalse(calc.isCurrentYearLeapYear());
 //        System.out.println(ano);
     }
 
     @Test
-    public void divisivelPor_quatro_cem_MasNaoPor_quatrocentos_anoNormal() {
+    public void divisivelPor_quatro_cem_MasNaoPor_quatrocentos() {
         ano = 2300;
         Assertions.assertFalse(calc.isCurrentYearLeapYear());
     }
 
     //Anos bisextos
     @Test
-    public void divisivelPor_quatro_MasNaoPor_Cem_anoBisexto() {
+    public void divisivelPor_quatro_MasNaoPor_Cem() {
         ano = 2016;
         Assertions.assertTrue(calc.isCurrentYearLeapYear());
     }
 
     @Test
-    public void divisivelPor_quatro_cem_quatrocentos_anoBisexto() {
+    public void divisivelPor_quatro_cem_quatrocentos() {
         ano = 2000;
         Assertions.assertTrue(calc.isCurrentYearLeapYear());
     }
