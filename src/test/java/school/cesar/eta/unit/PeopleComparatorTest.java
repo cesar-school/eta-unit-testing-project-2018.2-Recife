82c9f5d8d9ab6bd072a4b903d2550c69a56be618
alessandro meireles <alessandromeireles@gmail.com>
2020-03-28 20:16:19 -0300

    people comparate

--- src/test/java/school/cesar/eta/unit/PeopleComparatorTest.java
+++ src/test/java/school/cesar/eta/unit/PeopleComparatorTest.java
@@ -73,5 +73,102 @@ public class  PeopleComparatorTest {
         dummySecond.setBirthday(LocalDate.of(1980,05,29));
         Assertions.assertTrue(application.isSamePerson(dummy,dummySecond));
     }
+    @Test
+    public void compare_isSameFamilyErroButSamePerson_false() {
+        Person dummy = new Person();
+        Person dummySecond = new Person();
+        dummy.setName("Alessandro");
+        dummy.setLastName("Meireles");
+        dummy.setState("Pernambuco");
+        dummy.setCity("Recife");
+        dummy.setMaritalStatus("Casado");
+        dummy.setBirthday(LocalDate.of(1980,05,29));
+        dummySecond.setName("Alessandro");
+        dummySecond.setLastName("Meireles");
+        dummySecond.setState("Pernambuco");
+        dummySecond.setCity("Recife");
+        dummySecond.setMaritalStatus("Casado");
+        dummySecond.setBirthday(LocalDate.of(1980,05,29));
+        Assertions.assertFalse(application.isSameFamily(dummy,dummySecond));
+    }
+
+    @Test
+    public void compare_isSameFamily_true() {
+        Person dummy = new Person();
+        Person dummySecond = new Person();
+        dummy.setName("Alessandro");
+        dummy.setLastName("Meireles");
+        dummy.setState("Pernambuco");
+        dummy.setCity("Recife");
+        dummy.setMaritalStatus("Casado");
+        dummy.setBirthday(LocalDate.of(1980,05,29));
+        dummySecond.setName("Fabiana");
+        dummySecond.setLastName("Meireles");
+        dummySecond.setState("Pernambuco");
+        dummySecond.setCity("Recife");
+        dummySecond.setMaritalStatus("Casado");
+        dummySecond.setBirthday(LocalDate.of(1983,04,25));
+        Assertions.assertTrue(application.isSameFamily(dummy,dummySecond));
+    }
+
+    @Test
+    public void compare_isNotSameFamily_false() {
+        Person dummy = new Person();
+        Person dummySecond = new Person();
+        dummy.setName("Alessandro");
+        dummy.setLastName("Meireles");
+        dummy.setState("Pernambuco");
+        dummy.setCity("Recife");
+        dummy.setMaritalStatus("Casado");
+        dummy.setBirthday(LocalDate.of(1980,05,29));
+        dummySecond.setName("Fabiana");
+        dummySecond.setLastName("Sales");
+        dummySecond.setState("Pernambuco");
+        dummySecond.setCity("Recife");
+        dummySecond.setMaritalStatus("Casado");
+        dummySecond.setBirthday(LocalDate.of(1980,05,29));
+        Assertions.assertFalse(application.isSameFamily(dummy,dummySecond));
+    }
+
+
+    @Test
+    public void compare_isSameFamilyButOtherCity_false() {
+        Person dummy = new Person();
+        Person dummySecond = new Person();
+        dummy.setName("Alessandro");
+        dummy.setLastName("Meireles");
+        dummy.setState("Pernambuco");
+        dummy.setCity("Recife");
+        dummy.setMaritalStatus("Casado");
+        dummy.setBirthday(LocalDate.of(1980,05,29));
+        dummySecond.setName("Fabiana");
+        dummySecond.setLastName("Meireles");
+        dummySecond.setState("Pernambuco");
+        dummySecond.setCity("Olinda");
+        dummySecond.setMaritalStatus("Solteiro");
+        dummySecond.setBirthday(LocalDate.of(1983,04,25));
+        Assertions.assertFalse(application.isSameFamily(dummy,dummySecond));
+    }
+
+    @Test
+    public void compare_isSameFamilyButOtherState_false() {
+        Person dummy = new Person();
+        Person dummySecond = new Person();
+        dummy.setName("Alessandro");
+        dummy.setLastName("Meireles");
+        dummy.setState("Pernambuco");
+        dummy.setCity("Bonito");
+        dummy.setMaritalStatus("Casado");
+        dummy.setBirthday(LocalDate.of(1980,05,29));
+        dummySecond.setName("Fabiana");
+        dummySecond.setLastName("Meireles");
+        dummySecond.setState("Bahia");
+        dummySecond.setCity("Bonito");
+        dummySecond.setMaritalStatus("Solteiro");
+        dummySecond.setBirthday(LocalDate.of(1983,04,25));
+        Assertions.assertFalse(application.isSameFamily(dummy,dummySecond));
+    }
+
+
 
 }
