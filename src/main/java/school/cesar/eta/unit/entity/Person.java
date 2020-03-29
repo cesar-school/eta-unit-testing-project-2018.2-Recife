69c1ef63345293ee7c1232a97bb28b36cd96b853
claudia duque <claudia.duque@gmail.com>
2020-03-28 21:03:59 -0300

    PeopleComparatorTestImplemented

--- src/main/java/school/cesar/eta/unit/entity/Person.java
+++ src/main/java/school/cesar/eta/unit/entity/Person.java
@@ -57,4 +57,61 @@ public class Person {
     public void setState(String state) {
         this.state = state;
     }
+
+    public static class Builder {
+
+        private String name;
+        private String lastName;
+        private LocalDate birthday;
+        private String maritalStatus;
+        private String city;
+        private String state;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder birthday(LocalDate birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public Builder maritalStatus(String maritalStatus) {
+            this.maritalStatus = maritalStatus;
+            return this;
+        }
+
+        public Builder city(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Builder state(String state) {
+            this.state = state;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+
+    private Person(Builder builder) {
+
+        name = builder.name;
+        lastName = builder.lastName;
+        birthday = builder.birthday;
+        maritalStatus = builder.maritalStatus;
+        city = builder.city;
+        state = builder.state;
+
+
+    }
+
 }
