e97e909d9270b601a577310a40f8e4b176625de4
claudia duque <claudia.duque@gmail.com>
2020-04-04 20:27:39 -0300

    ComparatorUpdate

--- src/test/java/school/cesar/eta/unit/PeopleComparatorTest.java
+++ src/test/java/school/cesar/eta/unit/PeopleComparatorTest.java
@@ -19,132 +19,45 @@ public class PeopleComparatorTest {
     }
 
     @Test
-    public void isBirthday(){
-        Assertions.assertEquals(false, person.getBirthday()==LocalDate.now());
-    }
-
-    @Test
-    public void isBirthdayTrue(){
-
-        LocalDate date = LocalDate.parse("03/13/2020", formatter);
-        Assertions.assertEquals(true, person.getBirthday().getMonth()==date.getMonth() && person.getBirthday().getDayOfMonth()==date.getDayOfMonth() );
-    }
-
-    @Test
-    public void isBirthdayTrue_false(){
-
-        LocalDate date = LocalDate.parse("03/01/2020", formatter);
-        Assertions.assertEquals(false, person.getBirthday().getMonth()==date.getMonth() && person.getBirthday().getDayOfMonth()==date.getDayOfMonth() );
-    }
-
-    @Test
-    public void isBirthdayALeapYear(){
-        Assertions.assertEquals(false, person.getBirthday().isLeapYear() );
-    }
-
-    @Test
-    public void isSameFirstName_true(){
-        Person secondPerson = new Person.Builder().name("Vitor").lastName("Duque").birthday(LocalDate.parse("01/10/1981", formatter)).maritalStatus("married").city("Rio de Janeiro").state("Rio de Janeiro").build();
-        Assertions.assertEquals(true, person.getName().equals(secondPerson.getName())  );
-
-    }
-
-    @Test
-    public void isSameFirstName_false(){
-        Person secondPerson = new Person.Builder().name("Alessandro").lastName("Lima").birthday(LocalDate.parse("01/19/1984", formatter)).maritalStatus("married").city("Belo Horizonte").state("Minas Gerais").build();
-        Assertions.assertEquals(false, person.getName().equals(secondPerson.getName())  );
-    }
-
-    @Test
-    public void isSameLastName_true(){
+    public void isSameFamilyTrue(){
+        PeopleComparator comparator = new PeopleComparator();
         Person secondPerson = new Person.Builder().name("Vidiane").lastName("Lima").birthday(LocalDate.parse("02/23/1996", formatter)).maritalStatus("single").city("Recife").state("Pernambuco").build();
-        Assertions.assertEquals(true, person.getLastName().equals(secondPerson.getLastName()) );
+        Assertions.assertEquals(true, comparator.isSameFamily(person, secondPerson));
     }
 
     @Test
-    public void isSameLastName_false(){
-        Person secondPerson = new Person.Builder().name("Augusto").lastName("Cardoso").birthday(LocalDate.parse("06/23/1987", formatter)).maritalStatus("single").city("Curitiba").state("Paraná").build();
-        Assertions.assertEquals(false, person.getLastName().equals(secondPerson.getLastName()) );
-    }
-
-    @Test
-    public void isSameMaritalStatus_true(){
-        Person secondPerson = new Person.Builder().name("Izabela").lastName("Medeiros").birthday(LocalDate.parse("09/22/1959", formatter)).maritalStatus("single").city("João Pessoa").state("Paraíba").build();
-        Assertions.assertEquals(true, person.getMaritalStatus().equals(secondPerson.getMaritalStatus()) );
-    }
-
-    @Test
-    public void isSameMaritalStatus_false(){
-        Person secondPerson = new Person.Builder().name("Bruno").lastName("Ferreira").birthday(LocalDate.parse("08/14/1991", formatter)).maritalStatus("married").city("Cajamar").state("São Paulo").build();
-        Assertions.assertEquals(false, person.getLastName().equals(secondPerson.getLastName()) );
-    }
-
-    @Test
-    public void isSameBirthplace_true(){
-        Person secondPerson = new Person.Builder().name("Claudia").lastName("Costa").birthday(LocalDate.parse("12/22/1977", formatter)).maritalStatus("single").city("Recife").state("Pernambuco").build();
-        Assertions.assertEquals(true, person.getCity().equals(secondPerson.getCity()) && person.getState().equals(secondPerson.getState()) );
-    }
-
-    @Test
-    public void isSameBirthPlace_false(){
-        Person secondPerson = new Person.Builder().name("Carlos").lastName("Alencar").birthday(LocalDate.parse("05/08/1977", formatter)).maritalStatus("married").city("Feira Nova").state("Pernambuco").build();
-        Assertions.assertEquals(false, person.getCity().equals(secondPerson.getCity()) && person.getState().equals(secondPerson.getState()) );
-}
-    @Test
-    public void isSameBirthState_true(){
-        Person secondPerson = new Person.Builder().name("Vitor").lastName("Lima").birthday(LocalDate.parse("09/22/1957", formatter)).maritalStatus("married").city("Petrolina").state("Pernambuco").build();
-        Assertions.assertEquals(false, person.getCity().equals(secondPerson.getCity()) && person.getState().equals(secondPerson.getState()));
-    }
-
-    @Test
-    public void isSameBirthState_false(){
-        Person secondPerson = new Person.Builder().name("Vitor").lastName("Lima").birthday(LocalDate.parse("09/22/1957", formatter)).maritalStatus("married").city("Recife").state("Amazonas").build();
-        Assertions.assertEquals(false, person.getCity().equals(secondPerson.getCity()) && person.getState().equals(secondPerson.getState()));
+    public void isSameFamilyFalse(){
+        PeopleComparator comparator = new PeopleComparator();
+        Person secondPerson = new Person.Builder().name("Vitor").lastName("Matos").birthday(LocalDate.parse("08/24/1976", formatter)).maritalStatus("single").city("Recife").state("Pernambuco").build();
+        Assertions.assertEquals(false, comparator.isSameFamily(person, secondPerson));
     }
 
 
     @Test
-    public void isSamePerson(){
-        Person secondPerson = new Person.Builder().name("Vitor").lastName("Lima").birthday(LocalDate.parse("03/13/1995", formatter)).maritalStatus("single").city("Recife").state("Pernambuco").build();
-        Assertions.assertEquals(true, person.getName().equals(secondPerson.getName()) && person.getLastName().equals(secondPerson.getLastName()) && person.getBirthday().getMonth() == secondPerson.getBirthday().getMonth() && person.getBirthday().getDayOfMonth() == secondPerson.getBirthday().getDayOfMonth() && person.getBirthday().getYear() == secondPerson.getBirthday().getYear() &&person.getMaritalStatus().equals(secondPerson.getMaritalStatus()) && person.getCity().equals(secondPerson.getCity()) && person.getState().equals(secondPerson.getState()));
+    public void isSamePersonTrue(){
+        PeopleComparator comparator = new PeopleComparator();
+        Assertions.assertEquals(true, comparator.isSamePerson(person, person));
     }
 
     @Test
-    public void isDifferentFirstNameRestIsTheSame(){
-        Person secondPerson = new Person.Builder().name("Marcelo").lastName("Lima").birthday(LocalDate.parse("03/13/1995", formatter)).maritalStatus("single").city("Recife").state("Pernambuco").build();
-        Assertions.assertEquals(false, person.getName().equals(secondPerson.getName()) && person.getLastName().equals(secondPerson.getLastName()) && person.getBirthday().getMonth() == secondPerson.getBirthday().getMonth() && person.getBirthday().getDayOfMonth() == secondPerson.getBirthday().getDayOfMonth() && person.getBirthday().getYear() == secondPerson.getBirthday().getYear() &&person.getMaritalStatus().equals(secondPerson.getMaritalStatus()) && person.getCity().equals(secondPerson.getCity()) && person.getState().equals(secondPerson.getState()));
+    public void isSamePersonFalse(){
+        PeopleComparator comparator = new PeopleComparator();
+        Person secondPerson = new Person.Builder().name("Vitor").lastName("Matos").birthday(LocalDate.parse("09/12/1953", formatter)).maritalStatus("single").city("Olinda").state("Pernambuco").build();
+        Assertions.assertEquals(false, comparator.isSamePerson(person, secondPerson));
     }
 
     @Test
-    public void isDifferentLastSameRestIsTheSame(){
-        Person secondPerson = new Person.Builder().name("Vitor").lastName("Araujo").birthday(LocalDate.parse("03/13/1995", formatter)).maritalStatus("single").city("Recife").state("Pernambuco").build();
-        Assertions.assertEquals(false, person.getName().equals(secondPerson.getName()) && person.getLastName().equals(secondPerson.getLastName()) && person.getBirthday().getMonth() == secondPerson.getBirthday().getMonth() && person.getBirthday().getDayOfMonth() == secondPerson.getBirthday().getDayOfMonth() && person.getBirthday().getYear() == secondPerson.getBirthday().getYear() &&person.getMaritalStatus().equals(secondPerson.getMaritalStatus()) && person.getCity().equals(secondPerson.getCity()) && person.getState().equals(secondPerson.getState()));
+    public void isTodaysPersonBirthDayFalse(){
+        PeopleComparator comparator = new PeopleComparator();
+        Assertions.assertEquals(false, comparator.isTodayPersonsBirthDay (person));
     }
 
     @Test
-    public void isDifferentBirthdayRestIsTheSame(){
-        Person secondPerson = new Person.Builder().name("Vitor").lastName("Lima").birthday(LocalDate.parse("08/28/1994", formatter)).maritalStatus("single").city("Recife").state("Pernambuco").build();
-        Assertions.assertEquals(false, person.getName().equals(secondPerson.getName()) && person.getLastName().equals(secondPerson.getLastName()) && person.getBirthday().getMonth() == secondPerson.getBirthday().getMonth() && person.getBirthday().getDayOfMonth() == secondPerson.getBirthday().getDayOfMonth() && person.getBirthday().getYear() == secondPerson.getBirthday().getYear() &&person.getMaritalStatus().equals(secondPerson.getMaritalStatus()) && person.getCity().equals(secondPerson.getCity()) && person.getState().equals(secondPerson.getState()));
+    public void isTodaysPersonBirthDayTrue(){
+        PeopleComparator comparator = new PeopleComparator();
+        Person secondPerson = new Person.Builder().name("Vitor").lastName("Matos").birthday(comparator.getCurrentDate()).maritalStatus("single").city("Olinda").state("Pernambuco").build();
+        Assertions.assertEquals(true, comparator.isTodayPersonsBirthDay (secondPerson));
     }
-
-    @Test
-    public void isDifferentMaritalStatusRestIsTheSame(){
-        Person secondPerson = new Person.Builder().name("Vitor").lastName("Lima").birthday(LocalDate.parse("03/13/1995", formatter)).maritalStatus("married").city("Recife").state("Pernambuco").build();
-        Assertions.assertEquals(false, person.getName().equals(secondPerson.getName()) && person.getLastName().equals(secondPerson.getLastName()) && person.getBirthday().getMonth() == secondPerson.getBirthday().getMonth() && person.getBirthday().getDayOfMonth() == secondPerson.getBirthday().getDayOfMonth() && person.getBirthday().getYear() == secondPerson.getBirthday().getYear() &&person.getMaritalStatus().equals(secondPerson.getMaritalStatus()) && person.getCity().equals(secondPerson.getCity()) && person.getState().equals(secondPerson.getState()));
-    }
-
-    @Test
-    public void isDifferentBirthCityRestIsTheSame(){
-        Person secondPerson = new Person.Builder().name("Vitor").lastName("Lima").birthday(LocalDate.parse("03/13/1995", formatter)).maritalStatus("single").city("Feira Nova").state("Pernambuco").build();
-        Assertions.assertEquals(false, person.getName().equals(secondPerson.getName()) && person.getLastName().equals(secondPerson.getLastName()) && person.getBirthday().getMonth() == secondPerson.getBirthday().getMonth() && person.getBirthday().getDayOfMonth() == secondPerson.getBirthday().getDayOfMonth() && person.getBirthday().getYear() == secondPerson.getBirthday().getYear() &&person.getMaritalStatus().equals(secondPerson.getMaritalStatus()) && person.getCity().equals(secondPerson.getCity()) && person.getState().equals(secondPerson.getState()));
-    }
-
-    @Test
-    public void isDifferentBirthStateRestIsTheSame(){
-        Person secondPerson = new Person.Builder().name("Vitor").lastName("Lima").birthday(LocalDate.parse("03/13/1995", formatter)).maritalStatus("single").city("Recife").state("Goiás").build();
-        Assertions.assertEquals(false, person.getName().equals(secondPerson.getName()) && person.getLastName().equals(secondPerson.getLastName()) && person.getBirthday().getMonth() == secondPerson.getBirthday().getMonth() && person.getBirthday().getDayOfMonth() == secondPerson.getBirthday().getDayOfMonth() && person.getBirthday().getYear() == secondPerson.getBirthday().getYear() &&person.getMaritalStatus().equals(secondPerson.getMaritalStatus()) && person.getCity().equals(secondPerson.getCity()) && person.getState().equals(secondPerson.getState()));
-    }
-
 
 
 }
