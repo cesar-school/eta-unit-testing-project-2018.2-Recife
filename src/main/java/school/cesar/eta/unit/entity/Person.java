17e043191cca6e045d48807d901c352a650b54f1
Kleya Negromonte <jullyan.kleya@gmail.com>
2020-04-06 13:53:54 -0300

    Class Person

--- src/main/java/school/cesar/eta/unit/entity/Person.java
+++ src/main/java/school/cesar/eta/unit/entity/Person.java
@@ -3,58 +3,53 @@ package school.cesar.eta.unit.entity;
 import java.time.LocalDate;
 
 public class Person {
-    private String name;
-    private String lastName;
-    private LocalDate birthday;
-    private String maritalStatus;
-    private String city;
-    private String state;
+    private String name = "Jullyan";
+    private String lastName = "Negromonte";
+    private LocalDate birthday = LocalDate.now();
+    private String maritalStatus = "single";
+    private String city = "Recife";
+    private String state = "PE";
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Person setName(String name) {
         this.name = name;
+        return this;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
+    public Person setLastName(String lastName) {
         this.lastName = lastName;
+        return this;
     }
 
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getMaritalStatus() {
-        return maritalStatus;
-    }
-
-    public void setMaritalStatus(String maritalStatus) {
+    public Person setMaritalStatus(String maritalStatus) {
         this.maritalStatus = maritalStatus;
+        return this;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
+    public Person setCity(String city) {
         this.city = city;
+        return this;
     }
 
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
+    public Person setState(String state) {
         this.state = state;
+        return this;
     }
+
+    public Person setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+        return this;
+    }
+
+    public Person createPerson(){
+        Person person = new Person();
+        person.setName(this.name);
+        person.setLastName(this.lastName);
+        person.setMaritalStatus(this.maritalStatus);
+        person.setCity(this.city);
+        person.setState(this.state);
+        person.setBirthday(this.birthday);
+        return person;
+    }
+
+
 }
--- src/test/java/school/cesar/eta/unit/PeopleComparatorTest.java
+++ src/test/java/school/cesar/eta/unit/PeopleComparatorTest.java
@@ -2,38 +2,50 @@ package school.cesar.eta.unit;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import school.cesar.eta.unit.entity.Person;
 
+import java.time.Clock;
 import java.time.LocalDate;
-
 import static org.testng.Assert.*;
+
 
 public class PeopleComparatorTest {
 
-    @Test
-    public void isTodayPersonsBirthDayTrue(){
-        Person p = new Person();
-         p.setBirthday(LocalDate.now());
+    PeopleComparator peopleComparator;
+    PersonCreator personCreator;
+    LocalDate now = LocalDate.now();
 
-        PeopleComparator pc = new PeopleComparator();
-        boolean TodayPersonsBirthDay = pc.isTodayPersonsBirthDay(p);
-        assertTrue(TodayPersonsBirthDay);
-    }
 
-    @Test
-    public void isTodayPersonsBirthDayFalse(){
-        Person p = new Person();
-        p.setBirthday(LocalDate.of(2020,02,22));
-
-        PeopleComparator PeopleComparator = new PeopleComparator();
-        boolean TodayPersonBirthDayFalse = PeopleComparator.isTodayPersonsBirthDay(p);
-
-        assertFalse(TodayPersonBirthDayFalse);
-
+    @BeforeEach
+    public void Setup(){
+        peopleComparator = new PeopleComparator() {
+            @Override
+            public LocalDate getCurrentDate(){
+                return now;
+            }
+        };
+        personCreator = new PersonCreator();
     }
 
 
+    @Test
+    public void isPersonsBirthDay_true(){
+        Person person = personCreator.setBirthday(now).createPerson();
+        Assertions.assertTrue(peopleComparator.isTodayPersonsBirthDay(person));
 
+    }
+
+    @Test
+    public void isTodayPersonsBirthDay_False() {
+        Person p = new Person();
+        p.setBirthday(now.minusDays(5))
+
+        Assertions.assertTrue(PeopleComparator.isTodayPersonsBirthDay(p));
+
+    }
 
 }
+
