f350d7262c2f10d89bb6006aa3efb1a83be7ef6a
alessandro meireles <alessandromeireles@gmail.com>
2020-04-16 09:01:01 -0300

    commit

--- src/test/java/school/cesar/eta/unit/LeapYearCalculatorTest.java
+++ src/test/java/school/cesar/eta/unit/LeapYearCalculatorTest.java
@@ -3,6 +3,7 @@ package school.cesar.eta.unit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import java.time.Year;
 
 public class LeapYearCalculatorTest {
     @InjectMocks
@@ -10,10 +11,21 @@ public class LeapYearCalculatorTest {
 
 
    @Test
-    public void  isLeapYear_yearNonDivisibleByFour_false() {
+    public void  thisYear_IsLeapYear_true() {
     application = new LeapYearCalculator();
         Assertions.assertTrue(application.isCurrentYearLeapYear());
     }
+
+    @Test
+    public void compareYear_CurrentYearIsLeapYear_true (){
+        Assertions.assertEquals(Year.now().getValue(), new LeapYearCalculator().getCurrentYear());
+    }
+
+    @Test
+    public void compareYear_CurrentYearIsLeapYear_false (){
+        Assertions.assertNotEquals(Year.now().getValue()+1, new LeapYearCalculator().getCurrentYear());
+    }
+
 
 //
 //    @Test
