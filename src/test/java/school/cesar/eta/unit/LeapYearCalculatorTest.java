d24c428dc8439c8a62ca7eb8db069d0cce114191
alessandro meireles <alessandromeireles@gmail.com>
2020-04-16 09:13:21 -0300

    final

--- src/test/java/school/cesar/eta/unit/LeapYearCalculatorTest.java
+++ src/test/java/school/cesar/eta/unit/LeapYearCalculatorTest.java
@@ -9,7 +9,6 @@ public class LeapYearCalculatorTest {
     @InjectMocks
     LeapYearCalculator application;
 
-
    @Test
     public void  thisYear_IsLeapYear_true() {
     application = new LeapYearCalculator();
@@ -27,21 +26,4 @@ public class LeapYearCalculatorTest {
     }
 
 
-//
-//    @Test
-//    public void isLeapYear_yearDivisibleByFour_true() {
-//        Assertions.assertTrue(LeapYearCalculator.isCurrentYearLeapYear(4));
-//    }
-//
-//    @Test
-//    public void isLeapYear_yearDivisibleByAHundred_false() {
-//        Assertions.assertFalse(LeapYearCalculator.isCurrentYearLeapYear(100));
-//    }
-//
-//    @Test
-//    public void isLeapYear_yearDivisibleByFourHundreds_True() {
-//        Assertions.assertTrue(LeapYearCalculator.isCurrentYearLeapYear(400));
-//    }
-//
-//
 }
